7cafbd6b4996f31b08d623115dac7b1d271f4f1b
Mathews Musukuma <sikaili99@gmail.com>
2023-12-15 16:06:26 +0200

    minor updates

--- woo-broadpay.php
+++ woo-broadpay.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: Payment Gateway for BroadPay
- * Plugin URI: https://github.com/sparcopay/wc-broadpay-payment-gateway
+ * Plugin URI: https://github.com/broadpaypay/wc-broadpay-payment-gateway
  * Description: Accept Mobile Money and Credit Card payments.
  * Version: 1.0.0
  * Author: BroadPay
@@ -11,7 +11,7 @@
 
 
 /**
- * Class WC_Gateway_Sparco file.
+ * Class WC_Gateway_Broadpay file.
  *
  * @package WooCommerce\Broadpay
  */
@@ -23,9 +23,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 if (! in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) return;
 
 
-add_action('plugins_loaded', 'sparco_payment_init', 11);
+add_action('plugins_loaded', 'broadpay_payment_init', 11);
 
-function sparco_payment_init(){
+function broadpay_payment_init(){
     if(class_exists('WC_Payment_Gateway')){
 
 		require plugin_dir_path(__FILE__) . '/includes/class-wc-broadpay-payment-gateway.php';
@@ -34,11 +34,11 @@ function sparco_payment_init(){
 	}
 }
 
-add_filter('woocommerce_payment_gateways', 'add_to_woo_sparco_payment_gateway');
+add_filter('woocommerce_payment_gateways', 'add_to_woo_broadpay_payment_gateway');
 
-function add_to_woo_sparco_payment_gateway($gateways)
+function add_to_woo_broadpay_payment_gateway($gateways)
 {
-    $gateways[] = 'WC_Gateway_Sparco';
+    $gateways[] = 'WC_Gateway_Broadpay';
 
     return $gateways;
 }
